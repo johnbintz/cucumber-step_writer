2aed2699f6b7c15568ebbdae23b896b584538939
John Bintz <john@coswellproductions.com>
2012-11-29 06:41:32 -0500

    disable the directory opening by default

--- skel/step_writer.rb
+++ skel/step_writer.rb
@@ -1,10 +1,12 @@
 Cucumber::StepWriter.after_write do |dir|
-  %w{open xdg-open}.each do |cmd|
-    %x{which #{cmd}}
+  if false
+    %w{open xdg-open}.each do |cmd|
+      %x{which #{cmd}}
 
-    if $?.exitstatus
-      system %{#{cmd} #{dir}}
-      break
+      if $?.exitstatus
+        system %{#{cmd} #{dir}}
+        break
+      end
     end
   end
 end
